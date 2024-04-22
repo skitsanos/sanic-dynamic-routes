3d7727a97469970516bf7f9a63e9ce176225127c
skitsanos <info@skitsanos.com>
2024-04-22 16:23:04 +0300

    chore: tweaks in launcher

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
-from sanic import Sanic
-from sanic.response import text
+import logging
+import socket
+
+from sanic import Sanic, Request
 
 from utils.router import load_routes
 
+hostname = socket.gethostname()
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the desired logging level
+    format=f'%(asctime)s [%(levelname)s] {hostname} %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
 app = Sanic("MyHelloWorldApp")
+
+
+@app.on_request
+async def check_token(request: Request):
+    logging.debug(request.token)
 
 
 @app.on_response
@@ -16,3 +32,6 @@ async def custom_banner(request, response):
 #     return text("Hello, world.")
 
 load_routes(app, "routes")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, auto_reload=True)
