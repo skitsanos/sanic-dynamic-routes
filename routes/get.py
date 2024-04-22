3a9f369454aa312024de19182750e291cc91ef8a
skitsanos <info@skitsanos.com>
2024-04-22 15:42:20 +0300

    chore: reading request headers example

--- routes/get.py
+++ routes/get.py
@@ -1,5 +1,8 @@
-from sanic import text
+from sanic import json, Request
 
 
-async def handler(request):
-    return text("Hello, world")
+async def handler(request:Request):
+    return json({
+        "version": "1.0.0",
+        "headers": [h for h in request.headers],
+    })
